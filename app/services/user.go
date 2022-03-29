586ad64a6d185cacf32296d2c70a9be0d5d29a1e
ddchef <o.o@xjie.me>
2022-03-29 13:07:35 +0800

    feat: 修改用户字段

--- app/services/user.go
+++ app/services/user.go
@@ -16,19 +16,19 @@ var UserService = new(userService)
 
 // Register 注册
 func (userService *userService) Register(params request.Register) (err error, user models.User) {
-	var result = global.App.DB.Where("mobile = ?", params.Mobile).Select("id").First(&models.User{})
+	var result = global.App.DB.Where("username = ?", params.Username).Select("id").First(&models.User{})
 	if result.RowsAffected != 0 {
-		err = errors.New("手机号已存在")
+		err = errors.New("用户名已存在")
 		return
 	}
-	user = models.User{Name: params.Name, Mobile: params.Mobile, Password: utils.BcryptMake([]byte(params.Password))}
+	user = models.User{Username: params.Username, Mobile: params.Mobile, Password: utils.BcryptMake([]byte(params.Password))}
 	err = global.App.DB.Create(&user).Error
 	return
 }
 
 // Login 登录
 func (userService *userService) Login(params request.Login) (err error, user *models.User) {
-	err = global.App.DB.Where("mobile = ?", params.Mobile).First(&user).Error
+	err = global.App.DB.Where("username = ?", params.Username).First(&user).Error
 	if err != nil || !utils.BcryptMakeCheck([]byte(params.Password), user.Password) {
 		err = errors.New("用户名不存在或密码错误")
 	}
